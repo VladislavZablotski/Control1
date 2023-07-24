9c2ad4af07910a0eba7d7205754a0541bd413112
unknown <zab_vl@tutby>
2023-07-24 18:10:32 +0300

    Commit #3: Program code is added

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿string[] array = new string[7];
+int counter = 0;
+for (int i=0; i<array.Length; i++)
+{
+    Console.WriteLine($"Введите строку {i+1}");
+    array[i] = Console.ReadLine();
+    if (array[i].Length<=3)
+    {
+        counter++;
+    }
+}
+string[] array_sorted = new string[counter];
+int j = 0;
+int k = 0;
+while (j<10)
+{
+    if (array[j].Length<=3)
+    {
+        array_sorted[k] = array[j];
+        k++;
+    }
+    j++;
+}
+Console.WriteLine();
+Console.WriteLine($"Исходный массив: {String.Join(",", array)}");
+Console.WriteLine($"Отобранный массив: {String.Join(",", array_sorted)}");
